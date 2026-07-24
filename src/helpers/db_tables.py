1c3e43a96912c663033b59c6705aacf360776c2e
Kalamojo <kojo22444488888888@gmail.com>
2026-07-24 03:33:29 -0400

    Just opening table

--- src/helpers/db_tables.py
+++ src/helpers/db_tables.py
@@ -1,24 +1,11 @@
 from lancedb import AsyncTable
 from lancedb.db import AsyncConnection
 from lancedb.index import BTree
-import pyarrow as pa
 from config.constants import COVER_TABLE_NAME
 
 
 async def get_cover_table(db: AsyncConnection, clip_dim: int, tower_dim: int) -> AsyncTable:
-    cover_schema = pa.schema(
-        [
-            pa.field("cover_id", pa.int64(), nullable=False),
-            pa.field("book_id", pa.int64(), nullable=False),
-            pa.field("isbn_13", pa.string(), nullable=False),
-            pa.field("cover_url", pa.string(), nullable=False),
-            pa.field("embedding", pa.fixed_shape_tensor(pa.float32(), (clip_dim,)), nullable=True), #cover_embedding
-            #pa.field("tower_embedding", pa.list_(pa.float32(), tower_dim), nullable=True),
-        ]
-    )
-    cover_table = await db.create_table(
-        COVER_TABLE_NAME, schema=cover_schema, exist_ok=True
-    )
+    cover_table = await db.open_table(COVER_TABLE_NAME)
 
     id_stats = await cover_table.index_stats("cover_id_idx")
     if not id_stats:
