1dbd0df5311b49d780e1e805af5f6f9d232e4f65
Kalamojo <kojo22444488888888@gmail.com>
2026-07-24 02:48:27 -0400

    Fixed schema

--- src/config/constants.py
+++ src/config/constants.py
@@ -1,3 +1,4 @@
 HARDCOVER_URL = "https://api.hardcover.app/v1/graphql"
+COVER_TABLE_NAME = "covers"
 CLIP_DIM = 512
 USER_ID_DIM = 4
--- src/helpers/db_tables.py
+++ src/helpers/db_tables.py
@@ -2,6 +2,7 @@ from lancedb import AsyncTable
 from lancedb.db import AsyncConnection
 from lancedb.index import BTree
 import pyarrow as pa
+from config.constants import COVER_TABLE_NAME
 
 
 async def get_cover_table(db: AsyncConnection, clip_dim: int, tower_dim: int) -> AsyncTable:
@@ -11,18 +12,19 @@ async def get_cover_table(db: AsyncConnection, clip_dim: int, tower_dim: int) ->
             pa.field("book_id", pa.int64(), nullable=False),
             pa.field("isbn_13", pa.string(), nullable=False),
             pa.field("cover_url", pa.string(), nullable=False),
-            pa.field("cover_embedding", pa.fixed_shape_tensor(pa.float32(), (clip_dim,)), nullable=False),
-            pa.field("tower_embedding", pa.list_(pa.float32(), tower_dim), nullable=True),
+            pa.field("embedding", pa.fixed_shape_tensor(pa.float32(), (clip_dim,)), nullable=False), #cover_embedding
+            #pa.field("tower_embedding", pa.list_(pa.float32(), tower_dim), nullable=True),
         ]
     )
     cover_table = await db.create_table(
-        "covers", schema=cover_schema, exist_ok=True
+        COVER_TABLE_NAME, schema=cover_schema, exist_ok=True
     )
 
     id_stats = await cover_table.index_stats("cover_id_idx")
     if not id_stats:
         await cover_table.create_index("cover_id", config=BTree(), name="cover_id_idx")
 
-    #await cover_table.add_columns({"tower_embedding": f"arrow_cast(NULL, 'FixedSizeList({tower_dim}, Float32)')"})
+    await cover_table.add_columns({"tower_embedding": f"arrow_cast(NULL, 'FixedSizeList({tower_dim}, Float32)')"})
+    await cover_table.alter_columns([{"path": "embedding", "rename": "cover_embedding"}])
 
     return cover_table
