c00127643f6da8a6e140ca9447de96f67a9da246
Kalamojo <kojo22444488888888@gmail.com>
2026-08-05 03:46:33 -0400

    Using job role

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 locals {
   ecs_execution_role_arn = data.terraform_remote_state.bic_infra.outputs.ecs_execution_role_arn
+  job_role_arn = data.terraform_remote_state.bic_infra.outputs.job_role_arn
   s3_db_uri              = data.terraform_remote_state.bic_infra.outputs.s3_db_uri
   hardcover_secret_arn   = data.terraform_remote_state.bic_infra.outputs.hardcover_secret_arn
   rec_efs_system_id      = data.terraform_remote_state.bic_infra.outputs.rec_efs_system_id
@@ -14,6 +15,8 @@ resource "aws_batch_job_definition" "job" {
     image = data.aws_ecr_image.server_image.image_uri
 
     executionRoleArn = local.ecs_execution_role_arn
+
+    jobRoleArn = local.job_role_arn
 
     resourceRequirements = [
       {
