e6280ff871e7e908c728ee5508d2b5bbc9c1abfb
Kalamojo <kojo22444488888888@gmail.com>
2026-08-06 04:11:05 -0400

    Increasing tuning lr and epochs

--- src/jobs/create_tables.py
+++ src/jobs/create_tables.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from helpers.db_tables import get_db, get_cover_table, get_user_table, get_feedback_table
+from helpers.db_tables import get_db, get_cover_table, get_user_table, get_feedback_table, get_runlog_table
 
 logger = logging.getLogger(__name__)
 
@@ -15,9 +15,11 @@ async def create_tables(db_uri: str):
     cover_table_task = asyncio.create_task(get_cover_table(db))
     user_table_task = asyncio.create_task(get_user_table(db))
     feedback_table_task = asyncio.create_task(get_feedback_table(db))
+    runlog_table_task = asyncio.create_task(get_runlog_table(db))
 
     await cover_table_task
     await user_table_task
     await feedback_table_task
+    await runlog_table_task
 
     logger.info("Tables created")
